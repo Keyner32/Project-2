bb44927d6ec60238b578fc9a335578591c89b996
Gabe Keyner <holyground32@gmail.com>
2016-09-05 22:51:23 -0700

    search bar added to explore activity

--- app/src/main/java/com/example/gabekeyner/project_2/Explore.java
+++ app/src/main/java/com/example/gabekeyner/project_2/Explore.java
@@ -1,9 +1,16 @@
 package com.example.gabekeyner.project_2;
 
+import android.app.SearchManager;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.SearchView;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class Explore extends AppCompatActivity implements View.OnClickListener {
 
@@ -19,6 +26,8 @@ public class Explore extends AppCompatActivity implements View.OnClickListener {
         beerBtn.setOnClickListener(this);
         wineBtn.setOnClickListener(this);
         mixedDrinkBtn.setOnClickListener(this);
+
+        handleIntent(getIntent());
     }
 
     @Override
@@ -42,6 +51,33 @@ public class Explore extends AppCompatActivity implements View.OnClickListener {
 //                Intent intent2 = new Intent(this, "put your universal class here for mixed");
 //                startActivity(intent2);
 //                break;
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.options_menu, menu);
+
+        SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+
+        SearchView searchView = (SearchView) menu.findItem(R.id.search).getActionView();
+
+        searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
+
+        return true;
+    }
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        handleIntent(intent);
+    }
+
+    private void handleIntent(Intent intent) {
+
+        if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
+            String query = intent.getStringExtra(SearchManager.QUERY);
+            Toast.makeText(Explore.this,"Searching for "+query,Toast.LENGTH_SHORT).show();
         }
     }
 }
--- app/src/main/java/com/example/gabekeyner/project_2/MainActivity.java
+++ app/src/main/java/com/example/gabekeyner/project_2/MainActivity.java
@@ -7,6 +7,31 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+/*TODO REQUIREMENTS
+-[]Have a theme and display data related to that theme.
+-[]Allow the user to search by at least three different search criterias
+    Examples for a shopping app: name, price, availability, size, description, etc.
+-[]Show results in separate entries/pages to the user, including descriptions, images, and whatever else you like
+-[]Gather data for the search results from the database
+-[]Allow the user to add rows to a database, which they can view at any time
+-[]Incorporate Object Oriented principles into your app
+    e.g. create a class that defines all of the database entries and more specific classes that extend it.
+-[*]Contain code comments, explaining the functionality behind sections of code
+-[]Include at least 4 user stories in the readme.md
+-[]Include espresso tests for your app
+-[]Ensure that you add a sufficient number of tests for the features of each screen of your app
+
+Bonus:
+-[]Use relationships between tables (highly recommended)
+    e.g. create a review system for user feedback for each item (second table)
+-[]Incorporate Square's Picasso photo library
+-[]Incorporate Material Design principles into your app
+-[]Add more complexity to the user's searches (possibly allowing more than one search criteria at a time)
+-[]Add JUnit tests for your non-Android classes (i.e. not Activity or Fragment)
+
+
+ */
+
 public class MainActivity extends AppCompatActivity {
 
     //Declare your buttons
