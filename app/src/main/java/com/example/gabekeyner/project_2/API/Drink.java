6a3b7845e76836e8d41a516159ec113841303186
Gabe Keyner <holyground32@gmail.com>
2016-10-04 18:09:52 -0700

    added api tab

--- app/src/main/java/com/example/gabekeyner/project_2/API/Drink.java
+++
@@ -1,7 +0,0 @@
-package com.example.gabekeyner.project_2.API;
-
-/**
- * Created by GabeKeyner on 9/18/2016.
- */
-public class Drink {
-}
--- app/src/main/java/com/example/gabekeyner/project_2/API/apiActivity.java
+++ app/src/main/java/com/example/gabekeyner/project_2/API/apiActivity.java
@@ -6,39 +6,38 @@ import android.net.NetworkInfo;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.text.Selection;
+import android.util.Log;
 import android.view.View;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.gabekeyner.project_2.R;
-import com.google.gson.Gson;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
 import java.net.URL;
-import java.util.ArrayList;
 
 public class apiActivity extends AppCompatActivity {
 
-    public static final String API_URL ="http://localhost:3000/api/drinks";
+    public static final String API_URL ="https://salty-peak-57677.herokuapp.com/api/drinks";
 
-    private ArrayAdapter<Selection> adapter;
+    private TextView drinks;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_api);
 
-        adapter = new ArrayAdapter<Selection>(this, android.R.layout.simple_list_item_1, new ArrayList<Selection>());
-        ListView listView = (ListView)findViewById(R.id.listView);
-        listView.setAdapter(adapter);
+
+     drinks = (TextView) findViewById(R.id.listView);
 
 
         Button button = (Button)findViewById(R.id.show_api);
@@ -58,57 +57,74 @@ public class apiActivity extends AppCompatActivity {
 
     }
 
+    private String downloadURL (String myURL)throws IOException,JSONException {
+        InputStream inStream = null;
+        try {
+            java.net.URL url = new URL(myURL);
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("GET");
+            conn.setDoInput(true);
+            conn.connect();
+            inStream = conn.getInputStream();
+            String contentAsString = readIt(inStream);
+            String processedJson = parseJson(contentAsString);
+           return  processedJson;
+        }finally {
+            if(inStream !=null) {
+                inStream.close();
+            }
+        }
 
-    private class DownloadURLTask extends AsyncTask<String,Void,APISearchResult>{
+    }
+
+    private String parseJson(String contentAsString)throws JSONException {
+        StringBuilder drinkList = new StringBuilder();
+        JSONArray array = new JSONArray(contentAsString);
+        for (int i = 0; i<array.length(); i++){
+            JSONObject drink = array.getJSONObject(i);
+            String drinkName = drink.getString("drinkName");
+            String alcoholType = drink.getString("alcoholType");
+            Long abvPercent = drink.getLong("abvPercent");
+            String drinkDescription = drink.getString("drinkDescription");
+            drinkList.append(drinkName);
+            drinkList.append("\n"+ alcoholType);
+            drinkList.append("\n"+ abvPercent);
+            drinkList.append("\n"+ drinkDescription+"\n" +"\n");
+        }
+        return drinkList.toString();
+    }
+
+
+    private class DownloadURLTask extends AsyncTask<String,Void,String>{
 
         @Override
-        protected APISearchResult doInBackground(String... params) {
-
-            String data = null;
-            InputStream inStream = null;
-
-            URL url = null;
-
-            HttpURLConnection connection = null;
+        protected String doInBackground(String... urls) {
             try {
-                url = new URL(params[0]);
-                connection = (HttpURLConnection) url.openConnection();
-                connection.connect();
-                inStream = connection.getInputStream();
-                data = getInputData(inStream);
+                return downloadURL(urls[0]);
+            }catch (IOException e){
+                Log.d("IOException", e.getMessage());
+                return "Unable to retrieve web page. URL may be invalid";
+            }catch (JSONException e){
+                return "JSON parsing issue: " + e.getMessage();
             }
-            catch (MalformedURLException e) {
-                e.printStackTrace();
-            }
-            catch (IOException e) {
-                e.printStackTrace();
-            }
-
-            Gson gson = new Gson();
-            APISearchResult apiSearchResult = gson.fromJson(data, APISearchResult.class);
-            return apiSearchResult;
         }
         @Override
-        protected void onPostExecute(APISearchResult apiSearchResult) {
+        protected void onPostExecute(String result) {
 
-            adapter.clear();
-            adapter.addAll(apiSearchResult.getList());
-            super.onPostExecute(apiSearchResult);
+            drinks.setText(result);
         }
 
-        private String getInputData (InputStream inStream)throws IOException {
-            StringBuilder builder = new StringBuilder();
-            BufferedReader reader = new BufferedReader(new InputStreamReader(inStream));
+    }
+    public String readIt (InputStream inStream)throws IOException {
+        StringBuilder builder = new StringBuilder();
+        BufferedReader reader = new BufferedReader(new InputStreamReader(inStream));
 
-            String data = null;
+        String read;
 
-            while ((data = reader.readLine()) != null){
-                builder.append(data);
-            }
-
-            reader.close();
-
-            return builder.toString();
+        while ((read = reader.readLine()) != null){
+            builder.append(read);
         }
+
+        return builder.toString();
     }
 }
